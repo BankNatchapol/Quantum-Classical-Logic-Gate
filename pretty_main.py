23947ffe9bdcb314c6c87a60c3d0d112e4a116b4
BankNatchapol <natchapol.pat@gmail.com>
2021-01-05 10:41:00 +0700

    fixed not

--- pretty_main.py
+++ pretty_main.py
@@ -7,10 +7,13 @@ from tabulate import tabulate
 sbits = [0, 1]
 tbits = [(0, 0), (0, 1), (1, 0), (1, 1)]
 
-table = pd.DataFrame(columns=['bit', 'not'])
+classicalTable = pd.DataFrame(columns=['bit', 'not'])
+quantumTable = pd.DataFrame(columns=['bit', 'not'])
 for bit in sbits:
-    table = table.append({'bit': bit, 'not': classical.not_gate(bit)}, ignore_index=True)
-print(table,end="\n")
+    classicalTable = classicalTable.append({'bit': bit, 'not': classical.not_gate(bit)}, ignore_index=True)
+    quantumTable = quantumTable.append({'bit': bit, 'not': classical.not_gate(bit)}, ignore_index=True)
+print(tabulate(classicalTable, headers='keys', tablefmt='psql'))
+print(tabulate(quantumTable, headers='keys', tablefmt='psql'), end="\n")
 
 gates = [('and', classical.and_gate, quantum.and_gate),
          ('or', classical.or_gate, quantum.or_gate),
@@ -24,6 +27,6 @@ for i in range(len(gates)):
         quantumTable = quantumTable.append({'bit1': bit[0], 'bit2': bit[1], gates[i][0]: gates[i][2](bit[0], bit[1])}, ignore_index=True)
 
     print(tabulate(classicalTable, headers='keys', tablefmt='psql'))
-    print(tabulate(quantumTable, headers='keys', tablefmt='psql'))
-    print()
+    print(tabulate(quantumTable, headers='keys', tablefmt='psql'), end="\n")
+
 
