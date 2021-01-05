c4ee7e3b7c51e6d8bdd8ade4c16d5e50e61ab945
BankNatchapol <natchapol.pat@gmail.com>
2021-01-05 09:51:50 +0700

    pretty table

--- pretty_main.py
+++ pretty_main.py
@@ -2,6 +2,8 @@ import classical
 import quantum
 from TOKEN import ACCESS_TOKEN
 import pandas as pd
+from tabulate import tabulate
+
 sbits = [0, 1]
 tbits = [(0, 0), (0, 1), (1, 0), (1, 1)]
 
@@ -20,15 +22,8 @@ for i in range(len(gates)):
     for bit in tbits:
         classicalTable = classicalTable.append({'bit1': bit[0], 'bit2': bit[1], gates[i][0]: gates[i][1](bit[0], bit[1])}, ignore_index=True)
         quantumTable = quantumTable.append({'bit1': bit[0], 'bit2': bit[1], gates[i][0]: gates[i][2](bit[0], bit[1])}, ignore_index=True)
-    print(classicalTable)
-    print(quantumTable, end="\n")
 
-# print()
-# print(f"Classical NOT : {bit1}->{classical.not_gate(bit1)}")
-# print(f"Quantum   NOT : {bit1}->{quantum.not_gate(bit1)}\n")
-# print(f"Classical AND : ({bit1}, {bit2})->{classical.and_gate(bit1, bit2)}")
-# print(f"Quantum   AND : ({bit1}, {bit2})->{quantum.and_gate(bit1, bit2)}\n")
-# print(f"Classical OR  : ({bit1}, {bit2})->{classical.or_gate(bit1, bit2)}")
-# print(f"Quantum   OR  : ({bit1}, {bit2})->{quantum.or_gate(bit1, bit2)}\n")
-# print(f"Classical XOR : ({bit1}, {bit2})->{classical.xor_gate(bit1, bit2)}")
-# print(f"Quantum   XOR : ({bit1}, {bit2})->{quantum.xor_gate(bit1, bit2)}")
+    print(tabulate(classicalTable, headers='keys', tablefmt='psql'))
+    print(tabulate(quantumTable, headers='keys', tablefmt='psql'))
+    print()
+
